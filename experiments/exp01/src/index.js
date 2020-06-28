8f88beb104bd1dcc814f3e9b6ed06a4768c9bc52
joao tiago viegas <joao.viegas@maersk.com>
2020-06-28 16:07:17 +0100

    ...refactoring

---
+++ experiments/exp01/src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
+import { ApolloServer } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import Resolvers from './resolvers';
+import Schema from './schema';
+
+const app = express();
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+    }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(compress());
+app.use(cors());
+
+const root = path.join(__dirname, '../');
+
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(root, '/dist/client/index.html'));
+});
+
+app.listen(8000, () => console.log('Listening on port 8000!'));
